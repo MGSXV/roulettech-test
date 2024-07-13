bf0fdcc7e25988d49596695ba1bde55a36451f4c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-07-13 15:23:53 +0100

    ~fix: styling bugs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,25 @@
 export default {
 	content: [
 		"./src/**/*.{js,jsx,ts,tsx}",
+		"./index.html"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'title': ['"Roboto"', 'sans-serif'],
+				'body': ['"Nunito"', 'sans-serif'],
+			},
+			colors: {
+				black: '#000',
+				white: '#fff',
+				primary: '#1c1c1c',
+				background: '#fafaff',
+				success: '#0b6e4f',
+				info: '#00bcbf',
+				warning: '#f95738',
+				danger: '#dd1c1a'
+			}
+		},
 	},
 	plugins: [],
 }
